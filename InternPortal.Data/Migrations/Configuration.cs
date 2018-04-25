1506c6a73d3895191d4a01db4e8e9dc5d905b28a
Peter Mathieu <peteramathieu@gmail.com>
2018-04-25 10:30:20 -0700

    Add seed methods for users and roles.

--- InternPortal.Data/Migrations/Configuration.cs
+++ InternPortal.Data/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 namespace InternPortal.Data.Migrations
 {
+    using InternPortal.Data.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
 
     internal sealed class Configuration : DbMigrationsConfiguration<InternPortal.Data.Models.InternPortalContext>
     {
@@ -14,18 +17,47 @@ namespace InternPortal.Data.Migrations
 
         protected override void Seed(InternPortal.Data.Models.InternPortalContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            SeedRoles(context);
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            SeedUsers(context);
+        }
+
+        private void SeedRoles(IInternPortalContext context)
+        {
+            var appContext = new ApplicationDbContext();
+
+            var roleStore = new RoleStore<IdentityRole>(appContext);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            roleManager.Create(new IdentityRole { Name = "admin" });
+            roleManager.Create(new IdentityRole { Name = "intern" });
+        }
+
+        private void SeedUsers(IInternPortalContext context)
+        {
+            var appContext = new ApplicationDbContext();
+            var userStore = new UserStore<ApplicationUser>(appContext);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var roleStore = new RoleStore<IdentityRole>(appContext);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            if (!context.AspNetUsers.Any(u => u.Email == "peteramathieu@gmail.com"))
+            {
+                var user = new ApplicationUser { UserName = "peteramathieu@gmail.com",Email= "peteramathieu@gmail.com" };
+
+                userManager.Create(user, "p@ssw0rd");
+                userManager.AddToRole(user.Id, "admin");
+            }
+
+            if (!context.AspNetUsers.Any(u => u.Email == "peter_mathieu@yahoo.com"))
+            {
+                var user = new ApplicationUser { UserName = "peter_mathieu@yahoo.com", Email = "peter_mathieu@yahoo.com" };
+
+                userManager.Create(user, "p@ssw0rd");
+                userManager.AddToRole(user.Id, "intern");
+            }
+
         }
     }
 }
